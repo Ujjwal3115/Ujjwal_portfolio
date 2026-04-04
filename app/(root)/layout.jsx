71ac67cafdf206f8f99ebb26368080bd57bdc7df
Ujjwal <ujjwalverma3115@gmail.com>
2026-04-04 21:58:17 +0530

    fixed scrolling in the mobile view for the landing page

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,14 +1,35 @@
 "use client";
 import Sidebar from "@/components/Sidebar";
 import { FullPageProvider } from "@alvalens/react-fullpage-snap";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width: 767px)");
+
+		const applyMediaState = (event) => {
+			setIsMobile(event.matches);
+		};
+
+		setIsMobile(mediaQuery.matches);
+		mediaQuery.addEventListener("change", applyMediaState);
+
+		return () => {
+			mediaQuery.removeEventListener("change", applyMediaState);
+		};
+	}, []);
+
 	return (
 		<FullPageProvider
 			anchors={["home", "about", "projects", "contact"]}
 			scrollingSpeed={1000}
 			menu="#sidebar"
-			lockAnchors={false}>
+			lockAnchors={false}
+			keyboardScrolling={!isMobile}
+			touchScrolling={!isMobile}
+			wheelScrolling={!isMobile}>
 			<Sidebar />
 			{children}
 		</FullPageProvider>
